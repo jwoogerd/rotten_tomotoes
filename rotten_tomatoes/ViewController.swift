1308d26d13cf075715491218d3591a69b1e72b1e
Jayme Woogerd <jayme.woogerd@tufts.edu>
2015-04-15 21:09:26 -0700

    Initial Commit

---
+++ rotten_tomatoes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rotten_tomatoes
+//
+//  Created by Jayme Woogerd on 4/15/15.
+//  Copyright (c) 2015 Jayme Woogerd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
